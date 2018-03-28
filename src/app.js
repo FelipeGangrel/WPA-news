5b2634be4f59a678fee49a1516ad2aa847440823
Felipe Gangrel <felipefrog@gmail.com>
2018-03-27 22:12:33 -0300

    deploy

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ window.addEventListener('load', async e => {
 
     if ('serviceWorker' in navigator) {
         try {
-            navigator.serviceWorker.register('../sw.js');
+            navigator.serviceWorker.register('sw.js');
             console.log(`Service Worker registrado`);
         } catch (error) {
             console.log(`Falhou ao registrar Service Worker ${error.stack}`);
